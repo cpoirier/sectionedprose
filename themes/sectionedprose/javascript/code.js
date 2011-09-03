7e358459ac4dcc0618ee6b3a25d6539abc05bc9e
Chris Poirier <host@courage-my-friend.org>
2011-09-03 11:15:12 -0700

    Reworked code-block handler to bring it in line with the CSS and DOM structure. Also added a change to shift the line-height of the code when in focus, as vertical rhythm is less important than readability.

--- themes/sectionedprose/javascript/code.js
+++ themes/sectionedprose/javascript/code.js
@@ -3,29 +3,39 @@
 $(document).ready(
   function() 
   {
-    var examplar        = $('#content pre:first');
-    var initialWidth    = examplar.width(); 
-    var initialPosition = examplar.css('position');
-    var initialZIndex   = examplar.css('z-index');
+    var examplarPreCode   = $('#content pre code').first();
+    var examplarPre       = examplarPreCode.parent();
+    var initialWidth      = examplarPre.width(); 
+    var initialPosition   = examplarPreCode.css('position');
+    var initialZIndex     = examplarPreCode.css('z-index');
+    var initialLineHeight = examplarPreCode.css('line-height');
+    var initialFontSize   = parseInt(examplarPreCode.css('font-size'));
 
-    $('pre').hover(
+    $('pre code').hover(
       function() 
       {
         var body   = $('#body');
+        var pre    = $(this).parent();
         var margin = $(this).position().left - body.position().left;
         var space  = Math.round(body.width() - initialWidth - (margin * 2));
-        if( $(this).width() == initialWidth )
+        if( pre.width() == initialWidth )
         { 
-          $(this).width(initialWidth + space);
+          $(this).height($(this).height());
           $(this).css('position', 'relative');
           $(this).css('z-index', 100);
+          $(this).animate({width: initialWidth + space, lineHeight: initialFontSize * 1.5}, 'fast');
         }
       }, 
       function() 
       {
-        $(this).css('z-index' , initialZIndex  );
-        $(this).css('position', initialPosition);
-        $(this).width(initialWidth);
+        $(this).animate({width: initialWidth, lineHeight: initialLineHeight}, 'fast', 
+          function() 
+          {
+            $(this).css('z-index' , initialZIndex  );
+            $(this).css('position', initialPosition);
+            $(this).height('auto');
+          }
+        );
       }
     );
   }
